2d99c7fd86e45f25d09b1e45df17f501742103ef
Garren Smith <garren.smith@gmail.com>
2016-05-18 15:16:25 +0200

    working

--- app/addons/documents/jumptodoc.react.jsx
+++ app/addons/documents/jumptodoc.react.jsx
@@ -16,28 +16,60 @@ define([
   'react',
   'react-dom',
   'react-select',
-], (app, FauxtonAPI, React, ReactDOM, ReactSelect) => {
+  'lodash'
+], (app, FauxtonAPI, React, ReactDOM, ReactSelect, {debounce}) => {
 
-  const JumpToDoc = ({database, allDocs}) => {
-    const options = allDocs.map(doc => {
-      return {
-        value: doc.get('_id'),
-        label: doc.get('_id')
-      };
-    });
-    console.log('op', options);
-    return (
-      <div id="jump-to-doc-wrapper" class="input-append">
-        <ReactSelect
-          name="jump-to-doc"
-          placeholder="Document ID"
-          className="input-large"
-          options={options}
-          clearable={false}
-          onChange={(val) => {console.log('bbb', val);}} />
-      </div>
-    );
-  };
+  const JumpToDoc = React.createClass({
+    propTypes: {
+      database: React.PropTypes.object.isRequired,
+      allDocs: React.PropTypes.object.isRequired,
+    },
+    /*
+    This makes me so so so sad,
+    basically we have done something wrong with the positioning of our sidebar,
+    so the only way to get the dropdown to be visible is to make its position absolute
+    and position it relative to the control next to it.
+    */
+    positionSelect () {
+      const el = ReactDOM.findDOMNode(this);
+      const dimensions = $('.control-toggle-include-docs')[0].getBoundingClientRect();
+      $(el).find('.jump-to-doc').css({
+        position: 'absolute',
+        left: dimensions.left + 84 + 'px'
+      });
+    },
+
+    componentDidMount () {
+      this.positionSelect();
+       window.addEventListener("resize", debounce(() => this.positionSelect(), 150));
+    },
+
+    onChange ({value: docId}) {
+      var url = FauxtonAPI.urls('document', 'app', app.utils.safeURLName(this.props.database.id), app.utils.safeURLName(docId) );
+      FauxtonAPI.navigate(url, {trigger: true});
+    },
+
+    render () {
+      const {database, allDocs} = this.props;
+      const options = allDocs.map(doc => {
+        return {
+          value: doc.get('_id'),
+          label: doc.get('_id')
+        };
+      });
+      return (
+        <div id="jump-to-doc" class="input-append">
+          <ReactSelect
+            name="jump-to-doc"
+            placeholder="Document ID"
+            className="input-large jump-to-doc"
+            options={options}
+            clearable={false}
+            onChange={this.onChange} />
+        </div>
+      );
+    }
+  });
 
   return {
     JumpToDoc,
